7bd09b6f0e4af366934d7f67ef6dc0f17463e0d1
Will Reid <williamwreid@gmail.com>
2015-11-29 20:20:08 -0500

    Added polyfill to resolve navigator.getUserMedia issue. Think there's an issue trying to run code locally. Going to push project to CS50 ide to run on apache server.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,50 @@
 
-// Create a global context
+// create a global context
 context = new (window.AudioContext || window.webkitAudioContext)();
 
-function init(context){
-  
-  var p = navigator.mediaDevices.getUserMedia({audio: true});
-  p.then(function(stream){
-    source = context.createMediaStreamSource(stream);
-    source.connect(context.destination);
-  })
+navigator = window.navigator;
+
+navigator.getUserMedia = ( navigator.getUserMedia || 
+  navigator.webkitGetUserMedia ||
+  navigator.mozGetUserMedia || 
+  navigator.msGetUserMedia);
+
+
+function init(){
+  // poly fill 
+  // navigator.mediaDevices = navigator.mediaDevices || ((navigator.mozGetUserMedia || navigator.webkitGetUserMedia) ? {
+  //    getUserMedia: function(c) {
+  //      return new Promise(function(y, n) {
+  //        (navigator.mozGetUserMedia ||
+  //         navigator.webkitGetUserMedia).call(navigator, c, y, n);
+  //      });
+  //    }
+  // } : null);
+
+  // if (!navigator.mediaDevices) {
+  //   console.log("getUserMedia() not supported.");
+  //   return;
+  // }
+
+  // navigator.mediaDevices.getUserMedia({ audio: true })
+  // .then(function(stream) {
+  //   source = context.createMediaStreamSource(stream);
+  //   source.connect(context.destination);
+  // })
+  // .catch(function(e) {
+  //   console.log(e.name);
+  // });
+
+  var constraints = { audio: true };
+
+  navigator.getUserMedia(constraints, 
+    function(stream) {
+      source = context.createMediaStreamSource(stream);
+      source.connect(context.destination);
+    },
+    function(e){
+      console.log(e.name);
+    });
 
 }
 
