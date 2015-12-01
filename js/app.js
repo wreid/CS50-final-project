51072cc64764e1a54fcbb414b601c808c833a8ef
Will Reid <williamwreid@gmail.com>
2015-12-01 09:32:45 -0500

    Downloaded RecorderJS library. Added start/stop logic flow and buffer callback to play recorded audio.

--- js/app.js
+++ js/app.js
@@ -1,61 +1,71 @@
 
 // create a global context
 context = new (window.AudioContext || window.webkitAudioContext)();
+var recording = false;
 
 function init(){
-  // poly fill 
-  navigator.mediaDevices = navigator.mediaDevices || ((navigator.mozGetUserMedia || navigator.webkitGetUserMedia) ? {
-     getUserMedia: function(c) {
-       return new Promise(function(y, n) {
-         (navigator.mozGetUserMedia ||
-          navigator.webkitGetUserMedia).call(navigator, c, y, n);
-       });
-     }
-  } : null);
+    // poly fill 
+    navigator.mediaDevices = navigator.mediaDevices || ((navigator.mozGetUserMedia || navigator.webkitGetUserMedia) ? {
+        getUserMedia: function(c) {
+        return new Promise(function(y, n) {
+            (navigator.mozGetUserMedia ||
+            navigator.webkitGetUserMedia).call(navigator, c, y, n);
+        });
+        }
+    } : null);
 
-  if (!navigator.mediaDevices) {
-    console.log("getUserMedia() not supported.");
-    return;
-  }
+    if (!navigator.mediaDevices) {
+        console.log("getUserMedia() not supported.");
+        return;
+    }
 
-  var constraints = { audio: true };
+    var constraints = { audio: true };
 
-  navigator.mediaDevices.getUserMedia(constraints)
-  .then(function(stream) {
-    source = stream;
-  })
-  .catch(function(e) { //error checking
-    console.log(e.name);
-  });
+    navigator.mediaDevices.getUserMedia(constraints)
+    .then(function(stream) {
+        source = stream;
+    })
+    .catch(function(e) { //error checking
+        console.log(e.name);
+    });
 
-  document.querySelector('#start-recording').onclick = function() {  
-      startRecording(source);
-  };
+    document.querySelector('#start-recording').onclick = function() {  
+        if (!recording) {
+            startRecording(source);
+            recording = true;
+        }
+        else {
+            stopRecording;
+            recording = false;
+        }
+    };
 
 
 }
 function startRecording(stream){
-  // create new recorder
-  mediaRecorder = new MediaStreamRecorder(stream);
-  mediaRecorder.mimeType = 'audio/ogg';
-  mediaRecorder.audioChannels = 2;
-  mediaRecorder.ondataavailable = function (blob){
-    console.log("here!");
-  }
+    // create new recorder
+    var rec = new Recorder(stream);
+    rec.record();
 }
 
+function stopRecording(){
+    rec.stop();
+    rec.getBuffer(getBufferCallback);
+}
 
-function createOscillator(context) {
-  osc = context.createOscillator()
-  osc.frequency.value = 440           // set the frequency to 440 HZ
-  osc.connect(context.destination)    // set the output to speakers
-  osc.start(0)                        // start immediately
-  setTimeout(function() { osc.stop() }, 2000) // stop the oscillator 2 seconds from now
-  // osc.start(0) <--- this will cause an error! you need to create a new oscillator
+function getBufferCallback( buffers ) {
+    var newSource = audioContext.createBufferSource();
+    var newBuffer = audioContext.createBuffer( 2, buffers[0].length, audioContext.sampleRate );
+    newBuffer.getChannelData(0).set(buffers[0]);
+    newBuffer.getChannelData(1).set(buffers[1]);
+    newSource.buffer = newBuffer;
+
+    newSource.connect(audioContext.destination);
+    newSource.start(0);
 }
 
 // run intitialization on DOM load
 window.onload = function(){
-  console.log("finished loading");
-  init(context);
+    console.log("finished loading");
+    init(context);
 }
