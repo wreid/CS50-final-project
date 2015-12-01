529d7d55f88e16c685a66b4a7cdcba7c7d34b452
Will Reid <williamwreid@gmail.com>
2015-11-30 23:03:33 -0500

    Checked out app.js to version with window.onload function. Fixed issue wherein the site wouldn't request access to the mic.

--- js/app.js
+++ js/app.js
@@ -1,44 +1,39 @@
 
-// var create a global context
-var context = new (window.AudioContext || window.webkitAudioContext)();
+// create a global context
+context = new (window.AudioContext || window.webkitAudioContext)();
 
-// stereo
-var channels = 2;
+function init(){
+  // poly fill 
+  navigator.mediaDevices = navigator.mediaDevices || ((navigator.mozGetUserMedia || navigator.webkitGetUserMedia) ? {
+     getUserMedia: function(c) {
+       return new Promise(function(y, n) {
+         (navigator.mozGetUserMedia ||
+          navigator.webkitGetUserMedia).call(navigator, c, y, n);
+       });
+     }
+  } : null);
 
-// buffer at most ten seconds of audio
-var maxFrameCount = context.sampleRate * 10.0;
+  if (!navigator.mediaDevices) {
+    console.log("getUserMedia() not supported.");
+    return;
+  }
 
-var buffer = context.createBuffer(channels, maxFrameCount, context.sampleRate);
+  var constraints = { audio: true };
 
-// poly fill to set mediaDevices 
-navigator.mediaDevices = navigator.mediaDevices || ((navigator.mozGetUserMedia || navigator.webkitGetUserMedia) ? {
-   getUserMedia: function(c) {
-     return new Promise(function(y, n) {
-       (navigator.mozGetUserMedia ||
-        navigator.webkitGetUserMedia).call(navigator, c, y, n);
-     });
-   }
-} : null);
+  navigator.mediaDevices.getUserMedia(constraints)
+  .then(function(stream) {
+    source = stream;
+  })
+  .catch(function(e) { //error checking
+    console.log(e.name);
+  });
 
-if (!navigator.mediaDevices) {
-  console.log("getUserMedia() not supported.");
-  return;
+  document.querySelector('#start-recording').onclick = function() {  
+      startRecording(source);
+  };
+
+
 }
-
-var constraints = { audio: true };
-
-navigator.mediaDevices.getUserMedia(constraints)
-.then(function(stream) {
-  source = stream;
-})
-.catch(function(e) { //error checking
-  console.log(e.name);
-});
-
-document.querySelector('#start-recording').onclick = function() {  
-    startRecording(source);
-};
-
 function startRecording(stream){
   // create new recorder
   mediaRecorder = new MediaStreamRecorder(stream);
@@ -57,4 +52,10 @@ function createOscillator(context) {
   osc.start(0)                        // start immediately
   setTimeout(function() { osc.stop() }, 2000) // stop the oscillator 2 seconds from now
   // osc.start(0) <--- this will cause an error! you need to create a new oscillator
+}
+
+// run intitialization on DOM load
+window.onload = function(){
+  console.log("finished loading");
+  init(context);
 }
