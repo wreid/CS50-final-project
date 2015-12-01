b19add424f6e18968829d1d07d9d7beb60d9e749
Will Reid <williamwreid@gmail.com>
2015-12-01 14:55:53 -0500

    Testing change from https to ssh.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 
 // create a global context
-context = new (window.AudioContext || window.webkitAudioContext)();
-var recording = false;
+var context = new (window.AudioContext || window.webkitAudioContext)();
+var recorders = [];
+var recorderIndex = 0;
 
 function init(){
     // poly fill 
@@ -22,46 +23,59 @@ function init(){
     var constraints = { audio: true };
 
     navigator.mediaDevices.getUserMedia(constraints)
-    .then(function(stream) {
-        source = stream;
-    })
-    .catch(function(e) { //error checking
-        console.log(e.name);
-    });
+        .then(function( stream ) {
+            // globally defined source for startRecording()
+            source = context.createMediaStreamSource(stream);
+        })
+        .catch(function(e) { //error checking
+            console.log(e.name);
+        });
 
+    // recording flag
+    var recording = false;
+
+    // recording listener
     document.querySelector('#start-recording').onclick = function() {  
+        console.log(recording);
         if (!recording) {
             startRecording(source);
             recording = true;
+            console.log(recording);
         }
         else {
-            stopRecording;
+            stopRecording();
             recording = false;
+            console.log(recording);
         }
     };
 
-
 }
-function startRecording(stream){
+
+function startRecording(input){
     // create new recorder
-    var rec = new Recorder(stream);
-    rec.record();
+    recorders[recorderIndex] = new Recorder(input);
+    recorders[recorderIndex].record();
+    console.log("started recording");
 }
 
 function stopRecording(){
-    rec.stop();
-    rec.getBuffer(getBufferCallback);
+    recorders[recorderIndex].stop();
+    recorders[recorderIndex].getBuffer(getBufferCallback);
+    recorderIndex++;
+    console.log("stopped recording");
 }
 
-function getBufferCallback( buffers ) {
-    var newSource = audioContext.createBufferSource();
-    var newBuffer = audioContext.createBuffer( 2, buffers[0].length, audioContext.sampleRate );
+function getBufferCallback(buffers) {
+    console.log("called back");
+    var newSource = context.createBufferSource();
+    var newBuffer = context.createBuffer(2, buffers[0].length, context.sampleRate);
     newBuffer.getChannelData(0).set(buffers[0]);
     newBuffer.getChannelData(1).set(buffers[1]);
     newSource.buffer = newBuffer;
 
-    newSource.connect(audioContext.destination);
+    newSource.connect(context.destination);
     newSource.start(0);
+    console.log("end of callback");
 }
 
 // run intitialization on DOM load
